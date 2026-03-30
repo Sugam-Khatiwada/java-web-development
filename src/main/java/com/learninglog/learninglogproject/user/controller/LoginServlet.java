96601db588cb36b5cf4ca154187fbef092ec7f93
Sugam-Khatiwada <201203380+Sugam-Khatiwada@users.noreply.github.com>
2026-03-30 08:25:40 +0545

    feat: login servlet for user login

---
+++ src/main/java/com/learninglog/learninglogproject/user/controller/LoginServlet.java
@@ -0,0 +1,48 @@
+package com.learninglog.learninglogproject.user.controller;
+
+import com.learninglog.learninglogproject.user.model.dao.UserDao;
+import com.learninglog.learninglogproject.user.model.User;
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        req.getRequestDispatcher("pages/login.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        // Handle form submission from login.jsp here
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        if(email.isEmpty() || password.isEmpty()){
+            req.setAttribute("error","Please fill the all fields");
+            req.getRequestDispatcher("pages/login.jsp").forward(req, resp);
+        }
+
+        try{
+            // Call userdao's loginUser method
+            UserDao dao = new UserDao();
+            User userObj = dao.loginUser(email, password);
+            if(userObj == null){
+                req.setAttribute("error","Invalid email or password");
+                req.getRequestDispatcher("pages/login.jsp").forward(req, resp);
+            }
+            else{
+                // Login successful
+                
+            }
+        }
+        catch(Exception e){
+
+        }
+
+    }
+}
