ddb470ae5df7fc650a5e15f67e5d8bd1c7de1ecf
Sugam-Khatiwada <201203380+Sugam-Khatiwada@users.noreply.github.com>
2026-03-30 08:26:37 +0545

    feat: register servlet for user registration

--- src/main/java/com/learninglog/learninglogproject/user/controller/RegisterServlet.java
+++ src/main/java/com/learninglog/learninglogproject/user/controller/RegisterServlet.java
@@ -10,6 +10,11 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+import org.mindrot.jbcrypt.BCrypt;
+
+import com.learninglog.learninglogproject.user.model.User;
+import com.learninglog.learninglogproject.user.model.dao.UserDao;
+
 // Map this servlet to the URL pattern "/register"
 @WebServlet("/register")
 public class RegisterServlet extends HttpServlet {
@@ -25,5 +30,41 @@ public class RegisterServlet extends HttpServlet {
         // Forward the request and response to the JSP page
         // This means the URL stays the same but content is loaded from JSP
         rd.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        // Handle form submission from register.jsp here
+        // For example, you can retrieve form parameters and process registration logic
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+
+        if(name.isEmpty() || email.isEmpty() || password.isEmpty()){
+            req.setAttribute("error","Please fill the all fields");
+            req.getRequestDispatcher("pages/register.jsp").forward(req, resp);
+        }
+
+        User userObj = new User();
+        userObj.setName(name);
+        userObj.setEmail(email);
+        // userObj.setPassword(password);
+        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt()); // Hash the password using BCrypt
+        userObj.setPassword(hashedPassword);
+
+        UserDao userDao = new UserDao();
+        try {
+        boolean result = userDao.insertUser(userObj);
+        if(result==true){
+            resp.sendRedirect("login");
+        }else{
+            req.setAttribute("error","Something went wrong");
+            req.getRequestDispatcher("pages/register.jsp").forward(req, resp);
+        }
+        }catch(Exception e){
+            req.setAttribute("error", e.getMessage());
+            req.getRequestDispatcher("pages/register.jsp").forward(req, resp);
+        }
     }
 }
