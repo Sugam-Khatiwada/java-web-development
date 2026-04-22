10c704d3b206d361967a068fcca6ba314e842efb
Sugam-Khatiwada <201203380+Sugam-Khatiwada@users.noreply.github.com>
2026-04-22 17:26:27 +0545

    feat: Add AuthFilter for authentication

---
+++ src/main/java/com/learninglog/learninglogproject/filter/AuthFilter.java
@@ -0,0 +1,42 @@
+package com.learninglog.learninglogproject.filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebFilter("/*")
+public class AuthFilter implements Filter{
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        HttpSession session = httpRequest.getSession(false);
+        boolean isLoggedIn = (session != null && session.getAttribute("user") != null);
+
+        String uri = httpRequest.getRequestURI();
+
+        boolean isLoginPageOrRegisterPage = uri.contains("login") || uri.contains("register");
+
+        if(isLoggedIn && isLoginPageOrRegisterPage){
+            httpResponse.sendRedirect("/");
+            return;
+        }
+        if(isLoggedIn || isLoginPageOrRegisterPage){
+            chain.doFilter(request, response);
+        }else {
+            httpResponse.sendRedirect("/login");
+        }
+    }
+
+
+}
