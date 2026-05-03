66f64a5ab61aefaa83b85ddd553ef23a2b4653da
Sugam-Khatiwada <201203380+Sugam-Khatiwada@users.noreply.github.com>
2026-05-03 09:21:11 +0545

    feat: image store is done

---
+++ src/main/java/com/learninglog/learninglogproject/utils/imageUtils.java
@@ -0,0 +1,23 @@
+package com.learninglog.learninglogproject.utils;
+
+import java.io.File;
+import java.io.IOException;
+
+import jakarta.servlet.http.Part;
+
+public class imageUtils {
+    public static String writeImage(Part imagePart) throws IOException {
+        String uploadDir = "C:\\Users\\Acer\\Random projects\\learninglog\\src\\main\\webapp\\images";
+        
+        File imagesPath = new File(uploadDir);
+        if (!imagesPath.exists()) {
+            imagesPath.mkdirs();
+        }
+        
+        String fileName = imagePart.getSubmittedFileName();
+        imagePart.write( uploadDir + File.separator + fileName);
+        return "images" + File.separator + fileName;
+    }
+
+    
+}
